6bbce56d4c43bb360ea57a4900cb8ec0697c081b
jscienciadados <joaosouzaps9@gmail.com>
2021-03-19 00:38:02 +0000

    gerando as pstagens

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager 
 import os
 
 app = Flask(__name__)
@@ -7,6 +8,9 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "banana"
 db = SQLAlchemy(app) 
+login = LoginManager(app)
 
 from app import routes, models
+db.create_all()
--- app/models.py
+++ app/models.py
@@ -1,20 +1,27 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password = db.Column(db.String(64))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-def __repr__(self):
-    return '<User {}>'.format(self.username)
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username) 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) 
 
-def __repr__(self):
-    return '<Post {}>'.format(self.body) 
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)       
+              
+
